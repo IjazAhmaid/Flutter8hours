5aad3296be3231c9fcd5c0182b11ff6b2ed9ec72
IjazAhmaid <IjazAhmaid>
2021-11-19 22:21:10 +0500

    Day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:project8hours/utlis/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -15,9 +22,9 @@ class LoginPage extends StatelessWidget {
                 'assets/images/login.png',
                 fit: BoxFit.cover,
               ),
-              const Text(
-                "welcome",
-                style: TextStyle(
+              Text(
+                "welcome $name",
+                style: const TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
                 ),
@@ -33,6 +40,10 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: const InputDecoration(
                           hintText: "Enter Username", labelText: "UserName"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -42,13 +53,48 @@ class LoginPage extends StatelessWidget {
                     const SizedBox(
                       height: 40,
                     ),
-                    ElevatedButton(
+                    InkWell(
+                      onTap: () async {
+                        await Future.delayed(const Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        setState(() {
+                          changeButton = true;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        width: changeButton ? 45 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : const Text('login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                )),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            /* shape: changeButton
+                              ? BoxShape.circle
+                              : BoxShape.rectangle,
+                          */
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8)),
+                      ),
+                    )
+
+                    /* ElevatedButton(
                         style: TextButton.styleFrom(
                             minimumSize: const Size(150, 40)),
                         onPressed: () {
                           Navigator.pushNamed(context, MyRoutes.homeRoute);
                         },
-                        child: const Text('Login')),
+                        child: const Text('Login')),*/
                   ],
                 ),
               )
